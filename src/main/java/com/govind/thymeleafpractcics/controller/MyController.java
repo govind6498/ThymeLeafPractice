ffce84f4241571aeeccf98ad83c8e7c7334c4ca5
govind6498 <govindsingh6498@gmail.com>
2021-12-17 13:53:18 +0530

    ADDED:To Add iteration method in  Thymeleaf

--- src/main/java/com/govind/thymeleafpractcics/controller/MyController.java
+++ src/main/java/com/govind/thymeleafpractcics/controller/MyController.java
@@ -1,9 +1,12 @@
 package com.govind.thymeleafpractcics.controller;
 
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,5 +19,13 @@ public class MyController {
 		model.addAttribute("Name","Govind Singh");
 		model.addAttribute("CurrentDate",new Date().toLocaleString());
 		return "about";  //about.html
+	}
+	@GetMapping("/example-loop")
+	public String iterateHandler(Model m) {
+		String[] strArray= {"Ankita","Laxmi","Govind","Karan","Anlit","John"};
+		
+		List<String>names = Arrays.asList(strArray);
+		m.addAttribute("names",names);
+		return "iterate";
 	}
 }
